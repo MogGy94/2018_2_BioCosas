d64805e54ee0b5f1c0acef46bb0b874fce878668
Moggy94 <ecanonp@unal.edu.co>
2018-09-25 02:57:18 -0500

    big Change

---
+++ Parcial-1/Cañon/Code/P2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Hola \n Parcial 2"
+for j in  ../Data/*.txt
+do
+  #grep ">" $i
+  echo $j
+
+  #tail -25 $i
+  cat $j |
+  #awk {'print $1'}
+  #cut -d ')' -f1,2,3 --output-delimiter='\n'
+  # pasa a columna
+  tr -s ')'  '\n'|
+  tr -s '('  ' '|
+  grep "^."|
+
+  #cut -d'(' -f1
+  awk {'print $3" "$2" "$1'} |
+   #wc
+   sort -nr |
+   head -10
+  #sort -nr
+
+done
+echo -e "Hola \n Parcial 2"
+#read PASSWORD
+
+#tr -s ' '  '\n'
--- Taller02/Code/1d.sh
+++ Taller02/Code/1d.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 for i in ls ../Data/*.fasta
 do
 grep ">" $i
